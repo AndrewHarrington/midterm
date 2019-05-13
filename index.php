cdfe458a575851d017491f64da066a9c5527398d
Andrew Harrington <aharrington4@mail.greenriver.edu>
2019-05-13 17:11:50 -0400

    Added Form for part 2

--- index.php
+++ index.php
@@ -4,12 +4,20 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require ("vendor/autoload.php");
+session_start();
 
 $f3 = Base::instance();
 
 $f3->route('GET|POST /', function (){
     echo '<h1>Midterm Survey</h1>';
-    echo '<a href="survey.html">Take my midterm survey</a>';
+    echo '<a href="328/midterm/survey/">Take my midterm survey</a>';
+});
+
+$f3->route('GET|POST /survey', function (){
+    $boxes = array('a'=>'A', 'b'=>'B', 'c'=>'C');
+    $_SESSION['boxes'] = $boxes;
+    $thing = new Template();
+    echo $thing->render('views/survey.php');
 });
 
 $f3->run();
